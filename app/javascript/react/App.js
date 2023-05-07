b1e7059e8b5baac5ba4081686d89d3b9a2a21bcd
mule <belmeetmule@gmail.com>
2023-05-07 05:55:29 +0300

    Style greeting component

--- app/javascript/react/App.js
+++ app/javascript/react/App.js
@@ -4,15 +4,9 @@ import { Greeting } from './components/greeting/Greeting';
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>
-          Welcome to the Greeting App
-        </h1>
-        <p> You can see random greeting message by clicking the greeting button</p>
-        <Greeting />
-      </header>
-    </div>
+  <Routes>
+    <Route path="/" element={<Greeting />} />
+  </Routes>
   );
 }
 
